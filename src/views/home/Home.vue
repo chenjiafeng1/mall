489299a916ee2e0ab8033a2bb1800deeef976098
陈佳峰 <1600235041@qq.com>
2021-07-26 15:49:40 +0800

    封装navbar组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,27 @@
 <template>
-  <h2>首页</h2>
+  <nav-bar class="nav-home">
+    <div  slot="center">首页</div>
+  </nav-bar>
 </template>
 
 <script>
-export default {
+import NavBar from 'components/common/navbar/NavBar'
 
+export default {
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style>
-
+.nav-home {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
